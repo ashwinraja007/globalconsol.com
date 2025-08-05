1de3dee249f2f51f9b1874bef7baf34ea518ad78
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-08-05 19:08:24 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -64,7 +64,6 @@ const Index = () => {
       <AboutSection />
       <VisionMissionSection />
       <ServicesSection />
-      <WorkflowSection />
       <GlobalPresence />
       <ContactForm />
       <Footer />
