81a2a523d994b9f21f90bf20dcaa35098d25aa8c
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-08-05 18:27:09 +0530

    Update IndonesiaHome.tsx

--- src/pages/IndonesiaHome.tsx
+++ src/pages/IndonesiaHome.tsx
@@ -46,7 +46,6 @@ const IndonesiaHome = () => {
       <ServicesSection />
       <WorkflowSection />
       <GlobalPresence />
-      <UpdatesSection />
       <ContactForm />
       <Footer />
     </div>
