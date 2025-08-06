8605bcd4ec05fcf04f5296a7a465fa21677972cd
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-08-07 02:44:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -164,7 +164,7 @@ const App: React.FC = () => {
               ))}
 
               {/* Auth routes */}
-              <Route path="/login" element={<Login />} />
+              <Route path="/login" element={<AdminLogin />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
 
