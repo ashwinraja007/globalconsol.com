a4e89ccfd2d94fdf84068240d962934024902820
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-08-07 00:56:28 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://lbimikyjhyewlssnaegl.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiaW1pa3lqaHlld2xzc25hZWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNTA3NTYsImV4cCI6MjA2NzcyNjc1Nn0.CFRWvEcR9P5YNqHwI1wJ7qsCTuGTO_MwRAq5nhRr3xo"; // your anon key
+const SUPABASE_URL = "https://omqbvshzljogjloluafm.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tcWJ2c2h6bGpvZ2psb2x1YWZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwMjIyNjQsImV4cCI6MjA1NDU5ODI2NH0.CPFzgM6UVevRzH14vAWMNN4xn7g9SZmOY9pv1cRerU8"; // your anon key
 
 export const supabase = createClient<Database>(
   SUPABASE_URL,
