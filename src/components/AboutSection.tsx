ff4f6e23fbb80e005ee40657f888cb1bcb3cc167
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-08-12 15:45:25 +0530

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -69,14 +69,14 @@ const AboutSection = () => {
                     <img
                       alt="GC Sri Lanka ISO Certification"
                       className="rounded-lg shadow-lg w-40 h-auto object-contain"
-                      src="/srilanka.jpg"
+                      src="/iso2.gif"
                     />
                   </ScrollAnimation>
                   <ScrollAnimation delay={400} className="relative flex justify-center">
                     <img
                       alt="GC Sri Lanka Brand Leadership Award 2024"
                       className="rounded-lg  w-56 h-auto object-contain"
-                      src="/iso2.gif" 
+                      src="/srilanka.jpg" 
                     />
                   </ScrollAnimation>
                 </div>
