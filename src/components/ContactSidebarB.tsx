6c61e7597b39084db7f5d657293b97fc01133749
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-01-21 13:08:41 +0530

    Update ContactSidebarB.tsx

--- src/components/ContactSidebarB.tsx
+++ src/components/ContactSidebarB.tsx
@@ -45,6 +45,13 @@ code: "in",
     contacts: ["+91 11 493224477 / 48 /49"],
     email: "info@oecl.sg"
   }, {
+    name: "Punjab",
+    lat: 30.89135,
+    lng: 75.93255,
+    address: "No. 7A, G K Estate,Hari Nagar, Mundian Kalan,Chandigarh Road,Ludhiana, Punjab – 141015",
+    contacts: ["+91 62845 49881"],
+    email: "navjot.kohli@ggl.sg"
+  },{
     name: "Bangalore",
     lat: 13.01855,
     lng: 77.64191,
